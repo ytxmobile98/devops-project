560e127d617699bc3b1b102ad054f53ec4aae2ed
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2023-12-16 22:41:54 +0800

    style: `.tf` files indent 2 spaces

--- configs/provider.tf
+++ configs/provider.tf
@@ -1,13 +1,14 @@
 terraform {
-    required_providers {
-        tencentcloud = {
-            source = "tencentcloudstack/tencentcloud"
-        }
+  required_providers {
+    tencentcloud = {
+      source = "tencentcloudstack/tencentcloud"
+      version = "1.81.59"
     }
+  }
 }
 
 # Cretentials are stored as environment variables,
 # provided by ./credentials/tencent-cloud.env
 provider "tencentcloud" {
-    region = var.region
+  region = var.region
 }
--- configs/security-group.tf
+++ configs/security-group.tf
@@ -1,19 +1,19 @@
 resource "tencentcloud_security_group" "sg_gz" {
-    name = "sg-gz"
+  name = "sg-gz"
 }
 
 resource "tencentcloud_security_group_lite_rule" "sg_gz" {
-    security_group_id = tencentcloud_security_group.sg_gz.id
+  security_group_id = tencentcloud_security_group.sg_gz.id
 
-    ingress = [
-        "ACCEPT#192.168.1.0/24#80#TCP",
-        "DROP#8.8.8.8#80,90#UDP",
-        "ACCEPT#0.0.0.0/0#80-90#TCP",
-    ]
+  ingress = [
+    "ACCEPT#192.168.1.0/24#80#TCP",
+    "DROP#8.8.8.8#80,90#UDP",
+    "ACCEPT#0.0.0.0/0#80-90#TCP",
+  ]
 
-    egress = [
-        "ACCEPT#192.168.0.0/16#ALL#TCP",
-        "ACCEPT#10.0.0.0/8#ALL#ICMP",
-        "DROP#0.0.0.0/0#ALL#ALL",
-    ]
+  egress = [
+    "ACCEPT#192.168.0.0/16#ALL#TCP",
+    "ACCEPT#10.0.0.0/8#ALL#ICMP",
+    "DROP#0.0.0.0/0#ALL#ALL",
+  ]
 }
