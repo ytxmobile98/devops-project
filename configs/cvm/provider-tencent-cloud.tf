f559e0fe737fe37ad47a10b44f1a23441e23a794
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2023-12-18 06:52:28 +0800

    style: format tf config files

--- configs/cvm/provider-tencent-cloud.tf
+++ configs/cvm/provider-tencent-cloud.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     tencentcloud = {
-      source = "tencentcloudstack/tencentcloud"
+      source  = "tencentcloudstack/tencentcloud"
       version = "1.81.59"
     }
   }
