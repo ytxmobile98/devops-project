4ed290c1fc3390cdfe6d5254b32b399fabf27004
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2023-12-18 06:51:45 +0800

    feat: update security group rules, allow ICMP ping, HTTP/HTTPS access

--- configs/cvm/security-group.tf
+++ configs/cvm/security-group.tf
@@ -6,8 +6,12 @@ resource "tencentcloud_security_group_lite_rule" "sg_gz" {
   security_group_id = tencentcloud_security_group.sg_gz.id
 
   ingress = [
-    "ACCEPT#0.0.0.0/0#22#TCP", # Allow SSH access
+    "ACCEPT#0.0.0.0/0#ALL#ICMP", # Allow ping
+    "ACCEPT#0.0.0.0/0#22#TCP",   # Allow SSH access
+    "ACCEPT#0.0.0.0/0#80#TCP",   # Allow HTTP access
+    "ACCEPT#0.0.0.0/0#443#TCP",  # Allow HTTPS access
     "ACCEPT#0.0.0.0/0#6443#TCP", # For Kubernetes
+    "ACCEPT#0.0.0.0/0#ALL#TCP",  # Allow all TCP access
   ]
   egress = [
     "ACCEPT#0.0.0.0/0#ALL#ALL",
