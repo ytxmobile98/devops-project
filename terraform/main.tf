8563084393f9a03d44798eb59d5a914601c2fa98
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2023-12-20 23:54:59 +0800

    chore: k3s clusters

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,18 @@
+module "common" {
+  source = "./common"
+}
+
 module "cvm" {
   source = "./cvm"
+}
+
+module "k3s" {
+  source     = "./k3s"
+  depends_on = [ module.cvm ]
+
+  public_ip    = module.cvm.cvm_instances[0].public_ip
+  private_ip   = module.cvm.cvm_instances[0].private_ip
+  server_name  = module.common.k3s.server_names[0]
+  ssh_user     = module.common.cvm.connection.user
+  ssh_password = module.common.cvm.connection.password
 }
