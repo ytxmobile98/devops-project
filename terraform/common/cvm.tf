43978006a5f759fa5cf55ff4d9c37bc56cbfd783
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2023-12-22 11:06:48 +0800

    fix: CVM use `data` blocks

--- terraform/common/cvm.tf
+++ terraform/common/cvm.tf
@@ -5,18 +5,6 @@ output "cvm" {
     region            = "ap-hongkong"
     availability_zone = "ap-hongkong-2"
 
-    # Image ID
-    # See: https://cloud.tencent.com/document/api/213/15715
-    image_id = "img-487zeit5" # Ubuntu Server 22.04 LTS 64-bit
-
-    # Instance type
-    # See: https://cloud.tencent.com/document/api/213/15749
-    instance_type = "S5.LARGE16"
-
-    # Disk type
-    # See: https://cloud.tencent.com/document/product/362/16312
-    disk_type = "CLOUD_SSD"
-
     # Instance names
     instance_names = [
       "devops-hongkong-cvm-1",
--- terraform/cvm/cvm.tf
+++ terraform/cvm/cvm.tf
@@ -2,13 +2,37 @@ module "common" {
   source = "../common"
 }
 
+data "tencentcloud_availability_zones_by_product" "default" {
+  product = "cvm"
+}
+
+data "tencentcloud_images" "default" {
+  image_type = ["PUBLIC_IMAGE"]
+  os_name = "ubuntu"
+}
+
+data "tencentcloud_instance_types" "default" {
+  filter {
+    name   = "instance-family"
+    values = ["S5"]
+  }
+
+  filter {
+    name   = "zone"
+    values = [module.common.cvm.availability_zone]
+  }
+
+  cpu_core_count = 4
+  memory_size    = 16
+}
+
 resource "tencentcloud_instance" "cvm_1" {
   instance_name     = module.common.cvm.instance_names[0]
   availability_zone = module.common.cvm.availability_zone
-  image_id          = module.common.cvm.image_id
-  instance_type     = module.common.cvm.instance_type
+  image_id          = data.tencentcloud_images.default.images[0].image_id
+  instance_type     = data.tencentcloud_instance_types.default.instance_types[0].instance_type
 
-  system_disk_type = module.common.cvm.disk_type
+  system_disk_type = "CLOUD_SSD"
   system_disk_size = 100
 
   vpc_id    = tencentcloud_vpc.vpc_hongkong.id
